311080227fd036d96f167da4a0ae033dddbdc50d
Chirayu Desai <chirayudesai1@gmail.com>
2021-10-28 17:37:57 +0530

    shusky: Add privapp permissions for EuiccGoogle
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

--- device-shiba.mk
+++ device-shiba.mk
@@ -17,6 +17,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 # Inherit from zuma
 include device/google/zuma/common.mk
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.sensors-V2-ndk.vendor:64
